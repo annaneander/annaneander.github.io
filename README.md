7df704332a60d2e695d2079be85e859261b69d64
Anna Neander <neander@kth.se>
2022-01-25 13:43:18 +0100

    added test file + some small changes

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 
 The visualisation shows an overview of all the students using parallel coordinates that sorts up all attributes simultaneously.
-The colors indicate different majors and each scale shows levels of different skills (self-evaluated).
+The colors indicate different majors and each scale shows levels of different skills (self-evaluated). Skills are ordered in relation to each other, so nearby skills are closer.
 
 Use brushes to drag up and down on the scales to filter out unwanted data using the ‘brushing-and-linking’ technique (Gavriel Salvendy, 2012). Click to select a specific student's path in order to see their details (on demand).
--- group_vis.js
+++ group_vis.js
@@ -1,7 +1,6 @@
 <!-- Code from d3-graph-gallery.com -->
 
-// set the dimensions and margins of the graph
-
+// set dimensions and margins of the graph
 //TODO: sort out sizes according to viewport
 const margin = {
     top: 40,
@@ -13,9 +12,7 @@ const margin = {
   height = 400 - margin.top - margin.bottom;
 
 /*
-  8 majors, 8 colors, 55 ≈ 35% opacity.
-  Red, blue and yellow. Avoid green for color blindness
-  TODO predefine colors as a list/dict.
+  8 majors, 8 colors. Red, blue and yellow. Avoid green for color blindness. TODO predefine colors as a list/dict.
 */
 const color = d3.scaleOrdinal()
   .domain([
@@ -29,14 +26,14 @@ const color = d3.scaleOrdinal()
     "Business Administration, Finance, Law, Economics"
   ])
   .range([
-    "#8931ef55",
-    "#ff00bd55",
-    "#e1184555",
-    "#f2ca1955",
-    "#6e2c0055",
-    "#f57f1755",
-    "#1b4f7255",
-    "#0057e955"
+    "#8931ef",
+    "#ff00bd",
+    "#e11845",
+    "#f2ca19",
+    "#6e2c00",
+    "#f57f17",
+    "#1b4f72",
+    "#0057e9"
   ])
 
 /*
@@ -52,9 +49,10 @@ Name: Alien Armpit  Hex: #87e911   GREEN DO NOT USE
 */
 
 
-// append the svg object to the body of the page
-let svg = d3.select("#nice_viz")
+// append the paracord svg to the body of the page
+let paracord = d3.select("#nice_viz")
   .append("svg")
+  .style("background-color", "#fefefa") //maybe a good idea??
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -62,113 +60,134 @@ let svg = d3.select("#nice_viz")
     "translate(" + margin.left + "," + margin.top + ")");
 
 
+// append another svg to bottom for pie chart
+let svg_sum = d3.select("#summary")
+  .append("svg")
+  .style("background-color", "#fefefa")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height - 200 + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+
 // Parse the data from csv file. total 44 students
 // column 11 -22 (starts with 0)
-d3.csv("student_data.csv", function(error, data) {
-  if (error) throw error;
-  console.log(data)
 
-  const dimensions = []
-  /* get skills from data col 11 to 22 */
-  for (let i = 0; i <= 11; i++) {
-    dimensions[i] = data.columns[i + 11]
-  }
+//d3.csv("student_data.csv").then(data => {
+  d3.csv("test_data.csv", (error, data) => {
+    if (error) throw error;
+    console.log(data)
 
-  // For each dimension, a linear scale. Stored in a y object
-  let y = {}
-  for (i in dimensions) {
-    skill = dimensions[i]
-    y[skill] = d3.scaleLinear()
-      .domain([1, 10]) // --> Same axis range for each group
-      // --> different axis range for each group --> .domain( [d3.extent(data, function(d) { return +d[name]; })] )
-      .range([height, 0])
-  }
+    const dimensions = []
+    /* get skills from data col 11 to 22 */
+    for (let i = 0; i <= 11; i++) {
+      dimensions[i] = data.columns[i + 11]
+    }
 
-  // Build the X scale -> it find the best position for each Y axis
-  x = d3.scalePoint()
-    .range([0, width])
-    .domain(dimensions);
+    // For each dimension, a linear scale. Stored in a y object
+    let y = {}
+    for (i in dimensions) {
+      skill = dimensions[i]
+      y[skill] = d3.scaleLinear()
+        .domain([1, 10]) // --> Same axis range for each group
+        // --> different axis range for each group --> .domain( [d3.extent(data, function(d) { return +d[name]; })] )
+        .range([height, 0])
+    }
 
-  // Highlight the Major that is hovered
-  let highlight = function(d) {
+    // Build the X scale -> let it find the best position for each Y axis
+    x = d3.scalePoint()
+      .range([0, width])
+      .domain(dimensions);
 
-    sel_major = d.Major
+    // Highlight the student that is hovered
+    let highlight = function(d) {
+      sel_alias = d.Alias
+      sel_major = d.Major
+      //console.log(sel_alias + " : " + sel_major + " : " + color(sel_major));
 
-    // first every group turns grey
-    d3.selectAll(".line")
-      .transition().duration(200)
-      .style("stroke", "lightgrey")
-      .style("opacity", "0.2")
-    // Second the hovered specie takes its color
+      // first every group turns grey
+      d3.selectAll(".line")
+        .transition().duration(200)
+        .style("stroke", "lightgrey")
+        .style("opacity", "0.2")
 
-    //This does not seem to work
-    d3.selectAll("." + sel_major)
-      .transition().duration(200)
-      .style("stroke", color(sel_major))
-      .style("opacity", "1")
-    //this info could be displayed
-    console.log(sel_major + " : " + color(sel_major));
+      // Second the hovered alias gets color back
+      //let a= d3.selectAll("." + sel_major)
+      let a = d3.select("#" + sel_alias);
+      console.log(a);
+      a.transition().duration(200)
+        .style("stroke", color(d.Major))
+        .style("opacity", "0.5")
 
-  }
+      //this info could be displayed
 
-  // Unhighlight
-  let doNotHighlight = function(d) {
-    d3.selectAll(".line")
-      .transition().duration(200).delay(1000)
-      .style("stroke", function(d) {
-        return (color(d.Major))
+    }
+
+    // Unhighlight
+    let doNotHighlight = function(d) {
+      d3.selectAll(".line")
+        .transition().duration(200).delay(1000)
+        .style("stroke", function(d) {
+          return (color(d.Major))
+        })
+        .style("opacity", "0.5")
+    }
+
+    // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+    function path(d) {
+      return d3.line()(dimensions.map(function(p) {
+        return [x(p), y[p](d[p])];
+      }));
+    }
+
+    function clicked() {
+      console.log("click")
+    }
+
+    // Draw the lines
+    paracord
+      .selectAll("myPath")
+      .data(data)
+      .enter()
+      .append("path")
+      .attr("id", d => d.Alias)
+      // 2 classes for each line: 'line' and the group name
+      .attr("class", d => "line " + d.Major)
+      .attr("d", path)
+      .style("fill", "none")
+      .style("stroke", d => color(d.Major))
+      .style("opacity", "0.5")
+      .on("mouseover", highlight)
+      .on("mouseleave", doNotHighlight)
+      .on("click", clicked)
+
+
+    // Draw the axis:
+    paracord.selectAll("myAxis")
+      // For each dimension of the dataset I add a 'g' element:
+      .data(dimensions).enter()
+      .append("g")
+      .attr("class", "axis")
+      // translate this element to its right position on the x axis
+      .attr("transform", function(d) {
+        return "translate(" + x(d) + ")";
       })
-      .style("opacity", "1")
-  }
+      // And I build the axis with the call function
+      .each(function(d) {
+        d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d]));
+      })
+      // Add axis title
+      .append("text")
+      .style("text-anchor", "middle")
+      .attr("y", -9)
+      .text(function(d) {
+        return d;
+      })
+      .style("fill", "black")
 
-  // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
-  function path(d) {
-    return d3.line()(dimensions.map(function(p) {
-      return [x(p), y[p](d[p])];
-    }));
-  }
+    //svg_sum.()
+  })
 
-  // Draw the lines
-  svg
-    .selectAll("myPath")
-    .data(data)
-    .enter()
-    .append("path")
-    .attr("class", function(d) {
-      return "line " + d.Major
-    }) // 2 class for each line: 'line' and the group name
-    .attr("d", path)
-    .style("fill", "none")
-    .style("stroke", function(d) {
-      return (color(d.Major))
-    })
-    .style("opacity", 0.5)
-    .on("mouseover", highlight)
-    .on("mouseleave", doNotHighlight )
-
-
-
-  // Draw the axis:
-  svg.selectAll("myAxis")
-    // For each dimension of the dataset I add a 'g' element:
-    .data(dimensions).enter()
-    .append("g")
-    .attr("class", "axis")
-    // translate this element to its right position on the x axis
-    .attr("transform", function(d) {
-      return "translate(" + x(d) + ")";
-    })
-    // And I build the axis with the call function
-    .each(function(d) {
-      d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d]));
-    })
-    // Add axis title
-    .append("text")
-    .style("text-anchor", "middle")
-    .attr("y", -9)
-    .text(function(d) {
-      return d;
-    })
-    .style("fill", "black")
-
-})
+//  .catch(error =>
+  //  console.log("Error : " + error))

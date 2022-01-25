bfc47edbeffe8cd46c21760b02df1d4019ce76e0
Anna Neander <neander@kth.se>
2022-01-25 15:14:52 +0100

    updated index.html + added css

--- group_vis.js
+++ group_vis.js
@@ -75,7 +75,7 @@ let svg_sum = d3.select("#summary")
 // column 11 -22 (starts with 0)
 
 //d3.csv("student_data.csv").then(data => {
-  d3.csv("test_data.csv", (error, data) => {
+  d3.csv("student_data.csv", (error, data) => {
     if (error) throw error;
     console.log(data)
 
